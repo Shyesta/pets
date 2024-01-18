cde900ffcf7625c61f1204ac0cbb6af5a651abe9
Sage Bain <bain.sage@student.greenriver.edu>
2024-01-18 12:25:08 -0500

    Added home page view

--- index.php
+++ index.php
@@ -9,11 +9,9 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 $f3->route('GET /', function(){
-    echo 'pets';
-
     //Display a view page
-//    $view = new Template();
-//    echo $view;
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 $f3->run();
